2e49cb749163bc70c25fd49099dcae697cbd9c33
jcombari <jcombari@gmail.com>
2025-02-21 12:00:00 +0000

    20250221_Convert images into graphs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
-// assets/js/main.js
-
-$(function() {
-   $('#navbar-placeholder').load('navbar.html');
+// Esperamos a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function() {
+   // Usamos fetch para cargar el contenido del archivo navbar.html
+   fetch('assets/html/navbar.html')
+       .then(response => response.text())
+       .then(data => {
+           // Insertamos el contenido del archivo navbar.html en el contenedor
+           document.getElementById('navbar-container').innerHTML = data;
+       })
+       .catch(error => {
+           console.error("Error al cargar el menú:", error);
+       });
 });
